abc70e795136c0a0ea0d9bd266c2c66759ad4725
Dinko Sulicek <dinko.sulicek@predavaci.algebra.hr>
2023-06-28 20:54:49 +0200

    working solution

---
+++ src/component/AddTodoForm.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import FormControl from "react-bootstrap/FormControl";
+import InputGroup from "react-bootstrap/InputGroup";
+
+class AddTodoForm extends Component {
+  state = {
+    newItem: "",
+  };
+
+  ref = React.createRef();
+
+  handleChange = (event) => {
+    const newItem = event.target.value;
+    this.setState({ newItem });
+  };
+
+  handleAddTodoClick = (event) => {
+    event.preventDefault();
+
+    const { newItem } = this.state;
+    const { addTodo } = this.props;
+
+    if (!newItem || !newItem.trim()) {
+      return;
+    }
+
+    handle(newItem);
+
+    this.setState({ newItem: "" });
+
+    if (this.ref.current) {
+      this.ref.current.focus();
+    }
+  };
+
+  render() {
+    const { newItem } = this.state;
+
+    return (
+      <Form>
+        <InputGroup size="lg">
+          <FormControl
+            placeholder="Add todo"
+            onChange={this.handleChange}
+            value={newItem}
+            autoFocus
+            ref={this.ref}
+          />
+          <InputGroup>
+            <Button
+              variant="outline-secondary"
+              onClick={this.handleAddTodoClick}
+              type="submit"
+            >
+              Add
+            </Button>
+          </InputGroup>
+        </InputGroup>
+      </Form>
+    );
+  }
+}
+
+export default AddTodoForm;
